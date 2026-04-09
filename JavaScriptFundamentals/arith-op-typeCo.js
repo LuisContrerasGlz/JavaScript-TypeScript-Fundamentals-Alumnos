8f04258d643104b46b209ee50c0a5cdf2e9b08ef
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2026-04-08 21:37:54 -0600

    Op

---
+++ JavaScriptFundamentals/arith-op-typeCo.js
@@ -0,0 +1,54 @@
+/*
+
+// Operadores aritméticos
++
+-
+/
+*
+**
+%
+*/
+
+const a = 10
+const b = 3
+
+console.log(a + b) // 13
+console.log(a - b) // 7
+console.log(a * b) // 30
+console.log(a / b) // 3.3333333333333335
+console.log(a ** b) // 1000
+console.log(a % b) // 1
+
+// Operadores de asignación
+let x = 5
+x += 3 // x = x + 3
+console.log(x) // 8
+
+x -= 2 // x = x - 2
+console.log(x) // 6
+
+x *= 4 // x = x * 4
+console.log(x) // 24
+
+x /= 6 // x = x / 6
+console.log(x) // 4
+
+x **= 2 // x = x ** 2
+console.log(x) // 16
+
+x %= 5 // x = x % 5
+console.log(x) // 1     
+
+/*
+// Incremento y decremento
+--
+++
+*/
+
+let y = 10
+y++ // y = y + 1
+console.log(y) // 11
+
+y-- // y = y - 1
+console.log(y) // 10
+
