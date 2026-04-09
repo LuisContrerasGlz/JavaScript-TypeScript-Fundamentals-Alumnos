5337b496c701d4ae9b5f9012bae0cac4021b9448
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2026-04-08 18:40:51 -0600

    Template Strings

---
+++ JavaScriptFundamentals/comentarios-stringformat.js
@@ -0,0 +1,15 @@
+console.log("hello", "world", 3)
+console.error("error")
+console.warn("warning")
+
+// single line comment
+/* multi line comment
+hello
+*/
+
+const v = 64
+// Template Strings 
+console.log(`v=${v}`)
+
+const name = "Luis"
+console.log(`Hello ${name}`)
