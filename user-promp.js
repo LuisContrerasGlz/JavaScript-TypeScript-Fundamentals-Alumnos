0d3ac049c8d649415cf28f3b568d3fa2756416d3
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2026-04-08 18:36:58 -0600

    first commit

---
+++ user-promp.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+// Solicitar al usuario que ingrese su nombre
+const nombre = prompt('Por favor, ingresa tu nombre: ');
+
+// Saludar al usuario
+console.log(`¡Hola, ${nombre}! Bienvenido a la programación en JavaScript.`);   
