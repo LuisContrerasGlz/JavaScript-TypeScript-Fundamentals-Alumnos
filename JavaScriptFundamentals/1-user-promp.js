803ac54345355129b50b6efd9f506bcc63f524bf
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2026-04-11 08:54:05 -0600

    add more ex

--- JavaScriptFundamentals/1-user-promp.js
+++ JavaScriptFundamentals/1-user-promp.js
@@ -5,3 +5,14 @@ const nombre = prompt('Por favor, ingresa tu nombre: ');
 
 // Saludar al usuario
 console.log(`¡Hola, ${nombre}! Bienvenido a la programación en JavaScript.`);   
+
+// Otros ejemplos
+
+console.log("Hello, Automation!");
+
+console.info("This is info.");
+
+console.warn("Make sure to check the JS version and module settings.");
+
+console.error("If you encounter any issues, please report them to the development team.");
+
