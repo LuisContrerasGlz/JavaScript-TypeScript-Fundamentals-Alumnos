cb17718a13de4a7a266e4e38f20e354702221361
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2026-04-11 09:30:59 -0600

    TS Var

---
+++ TypeScriptFundamentals/3-booleans.ts
@@ -0,0 +1,11 @@
+let testPassed: boolean = true;
+let testFailed: boolean = false;
+
+console.log(`Test passed: ${testPassed}`);
+console.log(`Test failed: ${testFailed}`);
+
+testPassed = false;
+testFailed = true;
+
+console.log(`Test passed: ${testPassed}`);
+console.log(`Test failed: ${testFailed}`);
