a591f7d844274fd6f093edcd63a649e702909e6a
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2026-04-11 09:16:11 -0600

    ts

---
+++ TypeScriptFundamentals/hello.ts
@@ -0,0 +1,8 @@
+console.log("Hello, Automation!");
+
+console.info("This is a TypeScript project using.");
+
+console.warn("Make sure to check the TypeScript version and module settings.");
+
+console.error("If you encounter any issues, please report them to the development team.");
+
