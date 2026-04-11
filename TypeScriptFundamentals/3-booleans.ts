530dc7f04bf5b02fd38fec5e95a9b924d6d1b2d7
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2026-04-11 09:39:52 -0600

    Sim API ex

--- TypeScriptFundamentals/3-booleans.ts
+++ TypeScriptFundamentals/3-booleans.ts
@@ -9,3 +9,20 @@ testFailed = true;
 
 console.log(`Test passed: ${testPassed}`);
 console.log(`Test failed: ${testFailed}`);
+
+// Simulating API test
+
+const expectedStatusCode: number = 200;
+const actualStatusCode: number = 200;
+
+const isTestSuccessful: boolean = expectedStatusCode === actualStatusCode;
+
+console.log(`API Test successful: ${isTestSuccessful}`);
+
+// Simulating API test 2
+
+const expectedHttpCode: number = 200;
+const actualResult: number = 401;
+const assertionResult: boolean = expectedHttpCode === actualResult;
+
+console.log(`Assertion ${expectedHttpCode} and my received HTTP code is: ${actualResult} and the result is ${assertionResult}`);
