b4fc35a72d88829a3b1e20a0374c23b82d7c17bc
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2026-05-06 18:43:25 -0600

    basicsetup

---
+++ TypeScriptFundamentals/0-basicset.ts
@@ -0,0 +1,9 @@
+// Basic setup for a typescript project
+
+// Create a new project with npm init -y
+// Install typescript as a dev dependency with npm install typescript --save-dev
+// Create a tsconfig.json file with npx tsc --init
+// Compile the typescript file with npx tsc 0-basicset.ts
+// Run the compiled javascript file with node 0-basicset.js
+
+// This is a basic setup for a typescript project. You can add more files and compile them together with npx tsc. You can also use a build tool like webpack or rollup to bundle your files together.
