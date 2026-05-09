42b043c451a8c40519a1bc798c1931c3dda0fa2c
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2026-05-08 18:37:19 -0600

    ejercicios

---
+++ JavaScriptFundamentals/Ejercicios/1-encontrarlength_sinmetodo.js
@@ -0,0 +1,32 @@
+/*
+
+Find the length of a string(Without length): 
+input: "javascript" 
+output: 10
+
+*/
+
+const input = "javascript";
+
+let count = 0;
+
+for (const char of input) {
+  count++;
+}
+
+console.log(count);
+/*
+
+Time Complexity: O(n)
+
+Space Complexity: O(1)
+
+Time Complexity
+
+¿Cuánto trabajo hace?
+
+Space Complexity
+
+¿Cuánta memoria extra necesita?
+
+*/
